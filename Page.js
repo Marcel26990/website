ce736e6b9f5d8ab5c4ace40334c725b8894cb98d
Mark Reggiardo <markreggiardo@gmail.com>
2021-11-19 20:37:39 -0800

    ??

--- Page.js
+++ Page.js
@@ -1,5 +1,7 @@
-import { join } from "path";
+import path from "path";
 import JSONpages from "./pages.js";
+
+const __dirname = path.resolve();
 
 export default class Page {
     constructor(name, path, subPages = []) {
@@ -9,7 +11,7 @@ export default class Page {
     }
     
     publish(res) {
-        res.sendFile(join(__dirname, this.path))
+        res.sendFile(path.join(__dirname, this.path))
     }
     
     static getPages(pages = []) {
@@ -17,7 +19,7 @@ export default class Page {
             pages = JSONpages;
         }
         var returnArray = [];
-        Array.from(pages ?? []).forEach(object => {
+        Array.from(pages).forEach(object => {
             var subs = getPages(object.subPages) ?? [];
             let valueToAdd = new Page(object.name, object.path, subs);
             returnArray.push(valueToAdd);
