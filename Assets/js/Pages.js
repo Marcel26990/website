b2282d762b0230060cc98df93ac2a5870c91363e
Mark Reggiardo <markreggiardo@gmail.com>
2021-11-20 22:30:00 -0800

    ok?

--- Assets/js/Pages.js
+++ Assets/js/Pages.js
@@ -1,22 +1,21 @@
 import Page from './Page.js';
 
 function displayPages(pages) {
+    var output = "";
+    Page.getPages().forEach(page => {
+        output += `
+<div id="page">
+    <h1>${page.name}</h1>
+    <b>${page.path}</b>
+        `;
+        if (pages.subPages.length > 0) {
+            displayPages(page.subPages);
+        } else {
+            output += `</div>`
+        }
+    });
+    Document.getElementById("listOfPages").innerHTML = output;
     console.log("Function called!");
 }
 
 console.log("File called!");
-
-// var output = "";
-//     Page.getPages().forEach(page => {
-//         output += `
-// <div id="page">
-//     <h1>${page.name}</h1>
-//     <b>${page.path}</b>
-//         `;
-//         if (pages.subPages.length > 0) {
-//             displayPages(page.subPages);
-//         } else {
-//             output += `</div>`
-//         }
-//     });
-//     Document.getElementById("listOfPages").innerHTML = output;
